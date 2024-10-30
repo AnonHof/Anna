80fb6ab0383b5dc12e0ba111d5b746f46fbd6895
Mey <166723602+AnonHof@users.noreply.github.com>
2024-10-30 00:10:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal que redirige a "Inicio.html"
 @app.route("/")
 def index():
     return render_template("Inicio.html")
 
-# Ruta que redirige a "seleccion.html"
 @app.route("/seleccion")
 def seleccion():
     return render_template("seleccion.html")
+
+@app.route("/galeria")
+def galeria():
+    return render_template("galeria.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
